eed794f17ca0e651278d8e483ae8b728fb41aad8
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-07-23 18:02:16 +0200

    [Tantramour 2026] Mise à jour logistics.special.js

---
+++ logistics.special.js
@@ -0,0 +1,67 @@
+/**
+ * TANTRAMOUR 2026 — Informations Logistiques Spéciales (Supplémentaires)
+ */
+
+var LOGISTICS_SPECIAL = {
+
+  "JOUR2_REUNION_D_EQUIPE__HELPERS_": {
+    html: "<br><p><br></p>",
+    text: "",
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour1_TEST_MATT": {
+    html: "TEST SPECIAl",
+    text: "TEST SPECIAl",
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour1_PETIT_DEJ_2": {
+    html: "<br>",
+    text: "",
+    updatedAt: "2026-07-20"
+  },
+
+  "JOUR2_CESSER_DE_FAIRE__LAISSER_ETRE": {
+    html: "<p><br></p>",
+    text: "",
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour1_TEST_MATT_3": {
+    html: "test logistique special",
+    text: "test logistique special",
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR6_VOYAGE_AU_PAYS_DE_MON_SEXE": {
+    html: "<ul><li>Deux flacons de talc.</li></ul><div>*REMARQUE ;&nbsp;On serait plus confortables avec deux heures. Si galère, on s’adaptera avec 1h30.</div><p></p>",
+    text: "Deux flacons de talc.\n*REMARQUE ; On serait plus confortables avec deux heures. Si galère, on s’adaptera avec 1h30.",
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR4_LOVE_TEMPLE_DEVOTIONNEL": {
+    html: "<ul><li><font face=\"verdana, sans-serif\">3 grandes bassines (suffisamment larges pour les deux pieds).</font></li><li><font face=\"verdana, sans-serif\">3 Chaises associées.</font></li><li><font face=\"verdana, sans-serif\">6 petites serviettes</font></li><li><font face=\"verdana, sans-serif\">3 ou 4 tables basses</font></li><li><font face=\"verdana, sans-serif\">3 grands miroirs pleins pieds</font></li><li><font face=\"verdana, sans-serif\">Abondance d'encens, de bougies et de fleurs pour les offrandes</font></li><li><font face=\"verdana, sans-serif\">Une grande chaise type \"trône\"</font></li></ul><p></p>",
+    text: "3 grandes bassines (suffisamment larges pour les deux pieds).\n3 Chaises associées.\n6 petites serviettes\n3 ou 4 tables basses\n3 grands miroirs pleins pieds\nAbondance d'encens, de bougies et de fleurs pour les offrandes\nUne grande chaise type \"trône\"",
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR3_YONI___LINGAM_PUJA": {
+    html: "Besoin de 2 assistants avant pour la mise en place qui prend un peu de temps<br>(30 min si les assistants sont efficaces)<p></p>",
+    text: "Besoin de 2 assistants avant pour la mise en place qui prend un peu de temps\n(30 min si les assistants sont efficaces)",
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR6_PRANAM___DANSE_DEVOTIONNELLE": {
+    html: "o Micro casque<p></p>",
+    text: "o Micro casque",
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR2_SHAKTI_PUJA___LE_MYSTERE_DES_M": {
+    html: "o Micro casque si possible, sinon normal<br><div>o Tableau blanc pour écrire un mantra</div><div><br></div><div><b>Temps de préparation :</b><br>o Il y a besoin d’un temps de préparation et comme c’est le premier atelier le<br>matin après le meeting, j’aimerai bien accéder à la salle pendant le petit déj<br>pour faire la mise en place et avoir 1 ou 2 assistants à ce moment-là.</div><p></p>",
+    text: "o Micro casque si possible, sinon normal\n\no Tableau blanc pour écrire un mantra\n\n\nTemps de préparation :\no Il y a besoin d’un temps de préparation et comme c’est le premier atelier le\nmatin après le meeting, j’aimerai bien accéder à la salle pendant le petit déj\npour faire la mise en place et avoir 1 ou 2 assistants à ce moment-là.",
+    updatedAt: "2026-07-21"
+  }
+
+};
