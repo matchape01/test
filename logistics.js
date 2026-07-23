5cac13e058eed650884b90d421bafbeaa5a9090c
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-07-23 18:02:14 +0200

    [Tantramour 2026] Mise à jour logistics.js

--- logistics.js
+++ logistics.js
@@ -7,31 +7,134 @@ var LOGISTICS = {
   "J1_TEST_MATT": {
     html: "<p><strong>Besoins logistiques</strong></p><ul><li>Besoin logistique TEST avec</li><li>khdzlkhzesd</li><li>khzdlokfghzsd</li><li>khdlkhsdlkfg</li><li>khsdlkghsdlkhg</li><li>khdslokghsdlkhg</li><li>khsdkjoghlskdh</li><li>khsdolkghsdlkh</li><li>kjhsdlkghsdlkgh</li></ul><div><br></div><div><br></div><div><br></div><div>fghdfhdf</div>",
     text: "Besoins logistiquesBesoin logistique TEST aveckhdzlkhzesdkhzdlokfghzsdkhdlkhsdlkfgkhsdlkghsdlkhgkhdslokghsdlkhgkhsdkjoghlskdhkhsdolkghsdlkhkjhsdlkghsdlkghfghdfhdf",
+    validatedByFac: false,
     updatedAt: "2026-07-13"
   },
 
   "LOG_Jour1_TEST_MATT_2": {
     html: "TEST INFO LOGISTIQUE",
     text: "TEST INFO LOGISTIQUE",
+    validatedByFac: false,
     updatedAt: "2026-07-13"
   },
 
   "LOG_Jour1_TEST_3": {
     html: "Info logistique",
     text: "Info logistique",
+    validatedByFac: false,
     updatedAt: "2026-07-13"
   },
 
   "LOG_Jour1_TEST_5": {
     html: "<div>TEST k<u>hdlkghs</u>dkhg</div><div>dgsdg<b>sdgsdgds</b></div><div><ul><li><b>sdgsdg</b></li></ul></div>",
     text: "TEST khdlkghsdkhgdgsdgsdgsdgdssdgsdg",
+    validatedByFac: false,
     updatedAt: "2026-07-13"
   },
 
   "JOUR1_TEST_MATT_2__CHHANGEMENT____3_": {
     html: "<div>sdgsdg</div><div><b>sdgsdg</b></div><div><ul><li><b>dgsdg</b></li><li><b>sdgsdg</b></li></ul></div><p></p>",
     text: "sdgsdg\nsdgsdg\ndgsdg\nsdgsdg",
+    validatedByFac: false,
     updatedAt: "2026-07-13"
+  },
+
+  "JOUR2_RASSEMBLEMENT": {
+    html: "TEST INFO LOGISTIQUE<p></p>",
+    text: "TEST INFO LOGISTIQUE",
+    validatedByFac: false,
+    updatedAt: "2026-07-20"
+  },
+
+  "JOUR2_REUNION_D_EQUIPE__HELPERS_": {
+    html: "<br><p><br></p>",
+    text: "",
+    validatedByFac: false,
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour1_TEST_MATT": {
+    html: "sqfqs LOGISTIQUE",
+    text: "sqfqs LOGISTIQUE",
+    validatedByFac: false,
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour3_REUNION_D_EQUIPE__HELPERS_": {
+    html: "1",
+    text: "1",
+    validatedByFac: false,
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour1_PETIT_DEJ": {
+    html: "1",
+    text: "1",
+    validatedByFac: false,
+    updatedAt: "2026-07-20"
+  },
+
+  "JOUR2_CESSER_DE_FAIRE__LAISSER_ETRE": {
+    html: "<p><br></p>",
+    text: "",
+    validatedByFac: false,
+    updatedAt: "2026-07-20"
+  },
+
+  "LOG_Jour1_TEST_MATT_3": {
+    html: "test logistique",
+    text: "test logistique",
+    validatedByFac: true,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR5_ET_SI_TU_TISSAIS_LE_FIL_DE_TA_": {
+    html: "<ul><li>2 micros <b>sans fil </b>HF mains</li><li>Un matelas pour deux,&nbsp;</li><li>1 coussin mini par personne, voire 3 coussins pour deux pers</li></ul><p></p>",
+    text: "2 micros sans fil HF mains\nUn matelas pour deux, \n1 coussin mini par personne, voire 3 coussins pour deux pers",
+    validatedByFac: false,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR6_VOYAGE_AU_PAYS_DE_MON_SEXE": {
+    html: "<li>2 micros <b>sans fil </b>HF mains</li><li>Un matelas pour deux,&nbsp;</li><li>1 coussin mini par personne, voire 3 coussins pour deux pers</li><li>Deux flacons de talc. </li><p></p>",
+    text: "2 micros sans fil HF mains\nUn matelas pour deux, \n1 coussin mini par personne, voire 3 coussins pour deux pers\nDeux flacons de talc.",
+    validatedByFac: false,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR4_LOVE_TEMPLE_DEVOTIONNEL": {
+    html: "<div><font face=\"verdana, sans-serif\">Prévoit de <b>réunir les helpers 30 \nminutes avant le début du Temple</b> (ex: avant 11h30) pour finaliser \nl'installation et attribuer les rôles.</font></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\">Chaque helper devrait avoir un rôle précis pour l'explication des \"stations\" lors de l'introduction.</font></div><div><font face=\"verdana, sans-serif\"><b><br></b></font></div><div><font face=\"verdana, sans-serif\"><b>Objectif principal :</b> Aménager un espace\n dévotionnel centré sur un autel pour Shiva Nataraj, avec des zones de \nvénération et de confort.</font></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\"><u>Pour la vénération des pieds :</u><br><b>3 grandes bassines </b>(suffisamment larges pour les deux pieds).<br>Chaises associées.<br><b>6 petites serviettes</b> (idéalement fournies par \"Hamo\").</font></div><div><font face=\"verdana, sans-serif\"><br><u>Pour l'autel central (Shiva Nataraj) :</u><br>Une grande statuette de Shiva Nataraj (ou autre forme de Shiva).<br><b>3 ou 4 tables basses</b> (disponibles chez \"Hamo\") pour former la base de l'autel.<br>Des tissus pour couvrir les tables (apportés par Ishvari).<br>Abondance d'encens, de bougies et de fleurs pour les offrandes (le but est que Nataraj soit entouré de ces éléments).</font></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\"><u>Pour le confort et l'ambiance :</u><br><b>Matelas et tapis de yoga</b> (ceux déjà disponibles dans la salle devraient être suffisants).<br>De nombreux coussins.<br>Spots de lumière.<br>Des \"bandes d'eau pour ouvrir les yeux\" (détail non précisé).</font></div><div><font face=\"verdana, sans-serif\"><br><u>Mobilier et décor divers :</u><br><b>Une grande chaise type \"trône\"</b> (comme celle utilisée l'année précédente).<br><b>3 grands miroirs pleins pieds</b> (à vérifier si \"Hamo\" en dispose dans les chambres ou castings et s'ils sont faciles à déplacer).</font></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\"><b>Temps de préparation :</b><br>o Ça va demander pas mal de temps et perso j’aimerai bien être au concert de<br>Claude et Kelly, donc si possible, j’aimerai avoir accès à la salle Orion au<br>moment de la pause diner, ou dès que c’est possible en fin d’après-midi, pour<br>faire le set up avec quelques assistants. Probablement pour 1h.<br>o Il y a normalement 30 min de pause entre la fin du concert et le temple qui<br>commence à 23h30. J’aimerai avoir les assistants/helper donc à 23h pour<br>finaliser la mise en place.<br>o A voir si on laisse les portes du temple ouvert tout le temps ou si juste ouvert<br>au départ et ensuite fermé.<br>o J’aurai besoin des helper au départ pour faire des mini démo alors que<br>j’explique les différentes stations.</font></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\"><b>Matériels :</b><br><b>o Participants :</b><br></font><ul><li><font face=\"verdana, sans-serif\">Tenue blanche, sexy si possible ?</font></li><br></ul></div><div><ul><br></ul><font face=\"verdana, sans-serif\"><b>o Pour le temple :</b><br></font><ul><li><font face=\"verdana, sans-serif\">&nbsp;Micro</font></li><li><font face=\"verdana, sans-serif\">2 ou 3 bassines pour vénération des pieds</font></li><li><font face=\"verdana, sans-serif\">3 à 6 petites serviettes</font></li><li><font face=\"verdana, sans-serif\">3 chaises</font></li><li><font face=\"verdana, sans-serif\">Matelas</font></li><li><font face=\"verdana, sans-serif\">Tapis de yoga</font></li><li><font face=\"verdana, sans-serif\">1 statuette de shiva natarja ou autre divinité qui soit d’une assez</font></li><li><font face=\"verdana, sans-serif\">grande taille</font></li><li><font face=\"verdana, sans-serif\">2 ou 3 petite table base de coin (comme il y a d’habitude dans les</font></li><li><font face=\"verdana, sans-serif\">salles du hameau)</font></li><br></ul></div><div><ul><li><font face=\"verdana, sans-serif\"><b>Pour autel centrale :</b></font></li><li><font face=\"verdana, sans-serif\">&nbsp;Pleins de bougies chauffe plat pour offrande</font></li><li><font face=\"verdana, sans-serif\">Pleins d’encens</font></li><li><font face=\"verdana, sans-serif\">Pleins de fleurs</font></li><li><font face=\"verdana, sans-serif\">Quelques bandeaux pour couvrir les yeux</font></li><li><font face=\"verdana, sans-serif\">Grand miroir plein pied</font></li><li><font face=\"verdana, sans-serif\">Spot de lumière (1 ou 2)</font></li><li><font face=\"verdana, sans-serif\">Grande chaise comme un trône (si il y a)</font></li></ul></div><div><font face=\"verdana, sans-serif\"><br></font></div><div><font face=\"verdana, sans-serif\"><b>Chose que moi j’amène :</b><br>o Coupelle dorée<br>o Eau de rose<br>o Curcuma<br>o Tilak<br>o Gandam<br>o Poème dévotion<br>o Scotch sol<br>o Cymbale<br>o Fleurs séché<br>o Grand tissu pour derrière la vénération et délimitation<br>o Carte avec poème à l’entrée ?</font></div><p></p>",
+    text: "Prévoit de réunir les helpers 30 minutes avant le début du Temple (ex: avant 11h30) pour finaliser l'installation et attribuer les rôles.\n\n\nChaque helper devrait avoir un rôle précis pour l'explication des \"stations\" lors de l'introduction.\n\n\nObjectif principal : Aménager un espace dévotionnel centré sur un autel pour Shiva Nataraj, avec des zones de vénération et de confort.\n\n\nPour la vénération des pieds :\n3 grandes bassines (suffisamment larges pour les deux pieds).\nChaises associées.\n6 petites serviettes (idéalement fournies par \"Hamo\").\n\nPour l'autel central (Shiva Nataraj) :\nUne grande statuette de Shiva Nataraj (ou autre forme de Shiva).\n3 ou 4 tables basses (disponibles chez \"Hamo\") pour former la base de l'autel.\nDes tissus pour couvrir les tables (apportés par Ishvari).\nAbondance d'encens, de bougies et de fleurs pour les offrandes (le but est que Nataraj soit entouré de ces éléments).\n\n\nPour le confort et l'ambiance :\nMatelas et tapis de yoga (ceux déjà disponibles dans la salle devraient être suffisants).\nDe nombreux coussins.\nSpots de lumière.\nDes \"bandes d'eau pour ouvrir les yeux\" (détail non précisé).\n\nMobilier et décor divers :\nUne grande chaise type \"trône\" (comme celle utilisée l'année précédente).\n3 grands miroirs pleins pieds (à vérifier si \"Hamo\" en dispose dans les chambres ou castings et s'ils sont faciles à déplacer).\n\n\nTemps de préparation :\no Ça va demander pas mal de temps et perso j’aimerai bien être au concert de\nClaude et Kelly, donc si possible, j’aimerai avoir accès à la salle Orion au\nmoment de la pause diner, ou dès que c’est possible en fin d’après-midi, pour\nfaire le set up avec quelques assistants. Probablement pour 1h.\no Il y a normalement 30 min de pause entre la fin du concert et le temple qui\ncommence à 23h30. J’aimerai avoir les assistants/helper donc à 23h pour\nfinaliser la mise en place.\no A voir si on laisse les portes du temple ouvert tout le temps ou si juste ouvert\nau départ et ensuite fermé.\no J’aurai besoin des helper au départ pour faire des mini démo alors que\nj’explique les différentes stations.\n\n\n\n\nMatériels :\no Participants :\n\nTenue blanche, sexy si possible ?\n\n\n\n\no Pour le temple :\n\n Micro\n2 ou 3 bassines pour vénération des pieds\n3 à 6 petites serviettes\n3 chaises\nMatelas\nTapis de yoga\n1 statuette de shiva natarja ou autre divinité qui soit d’une assez\ngrande taille\n2 ou 3 petite table base de coin (comme il y a d’habitude dans les\nsalles du hameau)\n\n\nPour autel centrale :\n Pleins de bougies chauffe plat pour offrande\nPleins d’encens\nPleins de fleurs\nQuelques bandeaux pour couvrir les yeux\nGrand miroir plein pied\nSpot de lumière (1 ou 2)\nGrande chaise comme un trône (si il y a)\n\n\nChose que moi j’amène :\no Coupelle dorée\no Eau de rose\no Curcuma\no Tilak\no Gandam\no Poème dévotion\no Scotch sol\no Cymbale\no Fleurs séché\no Grand tissu pour derrière la vénération et délimitation\no Carte avec poème à l’entrée ?",
+    validatedByFac: false,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR3_YONI___LINGAM_PUJA": {
+    html: "<div><b>Besoin de 2 assistants avant pour la mise en place qui prend un peu de temps (30 min si les assistants sont efficaces)</b></div><div><b><br></b></div><div><b>Matériels :</b></div><ul><li>1 matelas pour 2</li><li>Coussins</li><li>Micro normal</li><li>Assistant.e</li><li>2 Briquets</li><li>Petit autel au centre de la pièce (je vais faire avec les choses que j’apporte)</li><li>Si possible tableau blanc pour écrire un mantra (mais si pas possible, pas de</li><li>problème)</li><li>Par couple/matelas :</li></ul><ol><li>1 petite assiette en carton pour poser les offrandes</li><li>2 petites bougie chauffe plat</li><li>1 encens</li><li>Fleurs ou pétales (il en faut quand même beaucoup)</li></ol><br><b>Temps de préparation</b><br><ul><li><b>Besoin de 2 assistants avant pour la mise en place qui prend un peu de temps (30 min si les assistants sont efficaces)</b></li><li>Pendant l’atelier, ca va dépendre du groupe, 1 assistant est suffisant si moins de 15 couples</li><li>Les participants arrivent déjà en pair et font tout la pratique ensemble. Check à l’entrée.</li></ul><div><br></div><div><b>Chose que moi j’amène :</b></div>o Petite coupelle dorée pour eau de rose<br>o Eau de rose<br>o Briquet<br>o Petite table autel au centre<br>o Bol dorée pour encens<br>o Fleur séché ?<br>o Tissu pour mini autel<p></p>",
+    text: "Besoin de 2 assistants avant pour la mise en place qui prend un peu de temps (30 min si les assistants sont efficaces)\n\n\nMatériels :\n1 matelas pour 2\nCoussins\nMicro normal\nAssistant.e\n2 Briquets\nPetit autel au centre de la pièce (je vais faire avec les choses que j’apporte)\nSi possible tableau blanc pour écrire un mantra (mais si pas possible, pas de\nproblème)\nPar couple/matelas :\n1 petite assiette en carton pour poser les offrandes\n2 petites bougie chauffe plat\n1 encens\nFleurs ou pétales (il en faut quand même beaucoup)\n\nTemps de préparation\n\nBesoin de 2 assistants avant pour la mise en place qui prend un peu de temps (30 min si les assistants sont efficaces)\nPendant l’atelier, ca va dépendre du groupe, 1 assistant est suffisant si moins de 15 couples\nLes participants arrivent déjà en pair et font tout la pratique ensemble. Check à l’entrée.\n\n\nChose que moi j’amène :\no Petite coupelle dorée pour eau de rose\no Eau de rose\no Briquet\no Petite table autel au centre\no Bol dorée pour encens\no Fleur séché ?\no Tissu pour mini autel",
+    validatedByFac: false,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR6_PRANAM___DANSE_DEVOTIONNELLE": {
+    html: "<b>Matériels :</b><br>o Coussins<br><div>o Micro casque</div><div><br></div><b>Temps de préparation</b><br><div>o Rien de particulier à prévoir.</div><div><br></div><b>Chose que moi j’amène :</b><br>o Petit autel pour centre<br>o Tissu<p></p>",
+    text: "Matériels :\no Coussins\n\no Micro casque\n\n\nTemps de préparation\n\no Rien de particulier à prévoir.\n\n\nChose que moi j’amène :\no Petit autel pour centre\no Tissu",
+    validatedByFac: false,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR2_SHAKTI_PUJA___LE_MYSTERE_DES_M": {
+    html: "<b>Matériels :</b><br>o Coussins<br>o Micro casque si possible, sinon normal<br>o Tableau blanc pour écrire un mantra<br>o Encens (1 boite)<br>o Bougies (1/participants)<br>o Boite de mouchoir ou lingette humide ca serai mieux<br>o Je ne sais pas encore exactement comment je vais le faire, je vais surement<br>faire comme une puja et/ou création de yantra avec riz ou graine, mais dans<br>ce cas-là, si c’est ok avec vous je m’occupe de les acheter moi-même. Ca sera<br>probablement : 2kg de riz blanc (si je fais le riz coloré),<br><br><b>Temps de préparation :</b><br>o Il y a besoin d’un temps de préparation et comme c’est le premier atelier le<br>matin après le meeting, j’aimerai bien accéder à la salle pendant le petit déj<br>pour faire la mise en place et avoir 1 ou 2 assistants à ce moment-là.<br><br><b>Ce que moi j’amène :</b><br>o Grand yantra<br>o Grand tissu<br>o Couleur pour riz ?<br>o Tali plate puja perso<br>o Gold Thai bol<br>o Règle<p></p>",
+    text: "Matériels :\no Coussins\no Micro casque si possible, sinon normal\no Tableau blanc pour écrire un mantra\no Encens (1 boite)\no Bougies (1/participants)\no Boite de mouchoir ou lingette humide ca serai mieux\no Je ne sais pas encore exactement comment je vais le faire, je vais surement\nfaire comme une puja et/ou création de yantra avec riz ou graine, mais dans\nce cas-là, si c’est ok avec vous je m’occupe de les acheter moi-même. Ca sera\nprobablement : 2kg de riz blanc (si je fais le riz coloré),\n\nTemps de préparation :\no Il y a besoin d’un temps de préparation et comme c’est le premier atelier le\nmatin après le meeting, j’aimerai bien accéder à la salle pendant le petit déj\npour faire la mise en place et avoir 1 ou 2 assistants à ce moment-là.\n\nCe que moi j’amène :\no Grand yantra\no Grand tissu\no Couleur pour riz ?\no Tali plate puja perso\no Gold Thai bol\no Règle",
+    validatedByFac: false,
+    updatedAt: "2026-07-21"
+  },
+
+  "JOUR2_MOVE___CONNECT_WITH_THE_5_ELEM": {
+    html: "- 2* micros<br>- coussins",
+    text: "- 2* micros\n- coussins",
+    validatedByFac: true,
+    updatedAt: "2026-07-23"
   }
 
 };
