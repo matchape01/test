55a5fe14a9f44b2f484cf9bb7b8cbd5fa4369931
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-07-31 02:44:53 +0200

    [Tantramour 2026] Mise à jour logistics.special.js

--- logistics.special.js
+++ logistics.special.js
@@ -1,5 +1,5 @@
 /**
- * TANTRAMOUR 2026 — Informations Logistiques Spéciales (Supplémentaires)
+ * TANTRAMOUR 2026 — Informations Logistiques Spéciales
  */
 
 var LOGISTICS_SPECIAL = {
@@ -7,7 +7,7 @@ var LOGISTICS_SPECIAL = {
   "JOUR2_REUNION_D_EQUIPE__HELPERS_": {
     html: "<br><p><br></p>",
     text: "",
-    updatedAt: "2026-07-20"
+    updatedAt: "2026-07-28"
   },
 
   "LOG_Jour1_TEST_MATT": {
@@ -29,9 +29,9 @@ var LOGISTICS_SPECIAL = {
   },
 
   "LOG_Jour1_TEST_MATT_3": {
-    html: "test logistique special",
-    text: "test logistique special",
-    updatedAt: "2026-07-21"
+    html: "test logistique special V4",
+    text: "test logistique special V4",
+    updatedAt: "2026-07-30 18:09"
   },
 
   "JOUR6_VOYAGE_AU_PAYS_DE_MON_SEXE": {
@@ -62,6 +62,78 @@ var LOGISTICS_SPECIAL = {
     html: "o Micro casque si possible, sinon normal<br><div>o Tableau blanc pour écrire un mantra</div><div><br></div><div><b>Temps de préparation :</b><br>o Il y a besoin d’un temps de préparation et comme c’est le premier atelier le<br>matin après le meeting, j’aimerai bien accéder à la salle pendant le petit déj<br>pour faire la mise en place et avoir 1 ou 2 assistants à ce moment-là.</div><p></p>",
     text: "o Micro casque si possible, sinon normal\n\no Tableau blanc pour écrire un mantra\n\n\nTemps de préparation :\no Il y a besoin d’un temps de préparation et comme c’est le premier atelier le\nmatin après le meeting, j’aimerai bien accéder à la salle pendant le petit déj\npour faire la mise en place et avoir 1 ou 2 assistants à ce moment-là.",
     updatedAt: "2026-07-21"
+  },
+
+  "LOG_Jour2_RASSEMBLEMENT": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour3_RASSEMBLEMENT": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour4_RASSEMBLEMENT": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour5_RASSEMBLEMENT": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour6_RASSEMBLEMENT": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour3_LOVE_TEMPLE_DES_EROTYPES__R": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour5_EMBRASSER_LE_MYSTERE": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour6_THE_TEMPLE_OF_EROTIC_INNOCENCE": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour6_SOIREE_SURPRISE_POUR_ENFLAMMER": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour7_CEREMONIE_DE_CLOTURE": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "LOG_Jour5_POOL_PARTY___DJ_JUAN_FELIX": {
+    html: "",
+    text: "",
+    updatedAt: "2026-07-28"
+  },
+
+  "JOUR1_REUNION_D_EQUIPE__HELPERS_": {
+    html: "qsfqf<p></p>",
+    text: "qsfqf",
+    updatedAt: "2026-07-31 02:44"
   }
 
 };
