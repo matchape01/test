4bdd3e213a32e26ce528404d879299e74cb64a43
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-18 18:59:52 +0200

    [Tantramour 2026] Mise à jour ref_news.js

--- ref_news.js
+++ ref_news.js
@@ -13,6 +13,5 @@
 
 var REF_NEWS = [
   {id:"NEWS_001",type:"dreamteam",fr:"Bienvenue dans l'espace DreamTeam du festival Tantramour 2026 ! Consultez régulièrement cette page pour les dernières annonces.",en:"Welcome to the DreamTeam space for Tantramour 2026! Check this page regularly for the latest announcements.",publishedAt:"2026-08-01 10:00",author:"Admin"},
-  {id:"NEWS_1787072226842",type:"dreamteam",fr:"dsdgs",en:"sdgsdg",publishedAt:"2026-08-18 18:57",author:"Admin"},
-  {id:"NEWS_1787072248243",type:"festivalier",fr:"<b>FESTIVALIER </b>FR",en:"<b>FESTIVALIEREng</b>",publishedAt:"2026-08-18 18:57",author:"Admin"}
+  {id:"NEWS_1787072391433",type:"dreamteam",fr:"TEST <b>DREAMTEAM</b> FR",en:"TEST <b>DREAMTEAM</b>&nbsp;ENg",publishedAt:"2026-08-18 18:59",author:"Admin"}
 ];
