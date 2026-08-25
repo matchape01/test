2b11134c48a40fa3304cc9ea60df4dda9ff7b873
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-25 22:13:11 +0200

    [TAF2026] Sync ref_equip_cat.js

--- ref_equip_cat.js
+++ ref_equip_cat.js
@@ -12,5 +12,5 @@ var REF_EQUIP_CAT = [
   { id: "CAT_05", value: "Consommables & fournitures", ordre: 5 },
   { id: "CAT_06", value: "Mobilier & aménagement", ordre: 6 },
   { id: "CAT_07", value: "Cuisine", ordre: 7 },
-  { id: "CAT_08", value: "Divers 2", ordre: 8 },
+  { id: "CAT_08", value: "Divers", ordre: 8 },
 ];
