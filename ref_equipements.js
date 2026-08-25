968c725bc6c94078451d7eda40c0b39fec1fac71
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-25 22:13:10 +0200

    [TAF2026] Sync ref_equipements.js

--- ref_equipements.js
+++ ref_equipements.js
@@ -65,6 +65,5 @@ var REF_EQUIPEMENTS = [
   { type: "Hameau", categorie: "Mobilier & aménagement", id: "EQ_NEW011", value: "Grande table pliante #1 (122 cm)", stock: 58, description: "Grande table pliante #1 (122 cm)", remarque: "Table grise pliante  (122cm/60cm/74cm hauteur)" },
   { type: "Tantramour", categorie: "Ambiance & décoration", id: "EQ_NEW02", value: "Encens", stock: 0, description: "Encens", remarque: "" },
   { type: "Tantramour", categorie: "Animation", id: "EQ_NEW03", value: "Masque", stock: 0, description: "Masque", remarque: "" },
-  { type: "Animateur", categorie: "Animation", id: "TEST_EQMATT", value: "TEST ITEM UPLAOD", stock: 56, description: "TEST ITEM UPLAOD", remarque: "" },
-  { type: "Animateur", categorie: "Animation", id: "TEST_EQMATT2", value: "TEST ITEM UPLAOD 2", stock: 56, description: "TEST ITEM UPLAOD", remarque: "" },
+  { type: "Autre", categorie: "Animation", id: "TEST_EQMATT", value: "TEST ITEM UPLAOD", stock: 56, description: "TEST ITEM UPLAOD", remarque: "" },
 ];
